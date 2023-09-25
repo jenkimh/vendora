65dfad6cbc20f65c842a716a85940c19cedd6ba9
Killian Kim <killianjyk@gmail.com>
2023-09-25 10:14:37 -0500

    created inventory folder, journal folder,and worked on two models: address and customer on sales_app

---
+++ journals/killian_kim.md
@@ -0,0 +1,2 @@
+# 9/21
+Parts of the model for Customer and Address completed for Inventory app.
--- sales/api/sales_app/models.py
+++ sales/api/sales_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Address(models.Model):
+    customer = models.ForeignKey(
+        'Customer',
+        on_delete=models.CASCADE,
+        related_name='addresses'
+    )
+    street_address = models.CharField(max_length=100)
+    apt_number = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"Address for {self.customer.first_name} {self.customer.last_name}"
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30, unique=True)
+    last_name = models.CharField(max_length=30, unique=True)
+    address = models.ForeignKey(
+        Address,
+        related_name="address",
+        on_delete=models.CASCADE,
+        null=True
+    )
+    phone_number = models.CharField(max_length=11, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_api_url(self):
+        return reverse("api_customer", kwargs={"pk": self.id})
+
+#might encounter circular import issues since
+#address and customer have two way relationship
