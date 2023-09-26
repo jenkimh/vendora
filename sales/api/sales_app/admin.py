6b974fc67dc96bb429831df8f485d7fd1290a134
ikranhaji <ikranhaji4@gmail.com>
2023-09-26 12:04:51 -0500

    added models and encoders for order, status

--- sales/api/sales_app/admin.py
+++ sales/api/sales_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Address
+from .models import Customer, Address, Order, Status
 # Register your models here.
 
 admin.site.register(Customer)
 admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(Status)
--- sales/api/sales_app/models.py
+++ sales/api/sales_app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+class CategoryVO(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class ProdcutVO(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.CharField(max_length=200)
+    image = models.ImageField()
+    category= models.ForeignKey(CategoryVO, related_name="items", on_delete=models.CASCADE)
+
 class Address(models.Model):
     customer = models.ForeignKey(
         'Customer',
@@ -34,5 +43,43 @@ class Customer(models.Model):
     def get_api_url(self):
         return reverse("api_customer", kwargs={"pk": self.id})
 
-#might encounter circular import issues since
-#address and customer have two way relationship
+class Status(models.Model):
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "statuses"
+
+
+class Payment(models.Model):
+    pass
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="customer")
+    product = models.ForeignKey(ProdcutVO, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length= 15)
+    ordered_date = models.DateTimeField()
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, default= "PENDING")
+    shipping_address = models.ForeignKey(Address, related_name="shipping_address", on_delete=models.CASCADE)
+    billing_address = models.ForeignKey(Address, related_name="billing_address", on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE , blank=True, null=True)
+
+    def ship(self):
+        status = Status.objects.get(name="SHIPPED")
+        self.status = status
+        self.save()
+
+    def deliver(self):
+        status = Status.objects.get(name="DELIVERED")
+        self.status = status
+        self.save()
+
+    def get_api_url(self):
+        return reverse("api_order", kwargs={"id": self.id})
+
+    def __str__(self):
+        return self.customer
