47b7a8bdf6c5c2d381009a5fc47c5f5c44f9fbe4
ikranhaji <ikranhaji4@gmail.com>
2023-10-02 09:15:37 -0500

    changed where images is stored once added on admin

--- inventory/api/inventory_app/models.py
+++ inventory/api/inventory_app/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     price = models.FloatField()
     description = models.CharField(max_length=200)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="images")
     category= models.ForeignKey(Category, related_name="items",
                                 on_delete=models.CASCADE)
     def __str__(self):
