24a1bb1e97e85ac3d84c7ac5a088d4e3ec0a6107
ikranhaji <ikranhaji4@gmail.com>
2023-10-10 12:29:39 -0500

    fixed and cleanedup urls, views for inventory

--- inventory/api/inventory_app/urls.py
+++ inventory/api/inventory_app/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
 
-from inventory_app.views import api_list_products, api_product_detail, api_list_categories
+from inventory_app.views import api_list_products, api_show_product, api_list_categories, api_show_category
 
 urlpatterns = [
     path("products/", api_list_products, name="api_list_products"),
     path(
         "products/<int:id>/",
-        api_product_detail,
-        name="api_product_detail",
+        api_show_product,
+        name="api_show_product",
     ),
      path(
         "category/",
         api_list_categories,
         name="api_list_categories",
+    ),
+    path(
+        "category/<int:id>/",
+        api_show_category,
+        name="api_show_category",
     ),
 
 ]
--- inventory/api/inventory_app/views.py
+++ inventory/api/inventory_app/views.py
@@ -24,47 +24,6 @@ def send_product_data(product):
     )
     connection.close()
 
-# def create_product(json_content):
-#     try:
-#         content = json.loads(json_content)
-#     except json.JSONDecodeError:
-#         return 400, {"message": "Bad JSON"}, None
-
-#     required_properties = [
-#         "title",
-#         "price",
-#         "description",
-#         "image",
-#         "category",
-#     ]
-#     missing_properties = []
-#     for required_property in required_properties:
-#         if (
-#             required_property not in content
-#             # or len(content[required_property]) == 0
-#         ):
-#             missing_properties.append(required_property)
-#     if missing_properties:
-#         response_content = {
-#             "message": "missing properties",
-#             "properties": missing_properties,
-#         }
-#         return 400, response_content, None
-#     try:
-#         product = Product.objects.create_product(
-#             title=content["title"],
-#             price=content["price"],
-#             description=content["description"],
-#             image=content["image"],
-#             category=content["category"],
-#         )
-#         return 200, product, product
-#     except IntegrityError as e:
-#         return 409, {"message": str(e)}, None
-#     except ValueError as e:
-#         return 400, {"message": str(e)}, None
-
-
 @require_http_methods(["GET", "POST"])
 def api_list_products(request):
     if request.method == "GET":
@@ -84,74 +43,40 @@ def api_list_products(request):
                 status=400,
             )
         product = Product.objects.create(**content)
-
-        # send_product_data(product)
         return JsonResponse(
             product,
             encoder=ProductEncoder,
             safe=False,
         )
-        # status_code, response_content = create_product(request.body)
-        # if status_code >= 200 and status_code < 300:
-        # response = JsonResponse(
-        #     response_content,
-        #     encoder=ProductEncoder,
-        #     safe=False,
-        # )
-        # response.status_code = status_code
-        # return response
-
-
 @require_http_methods(["GET", "PUT", "DELETE"])
-def api_product_detail(request, id):
-    try:
-        product = Product.objects.get(id=id)
-    except Product.DoesNotExist:
-        print("User.DoesNotExist", id)
-        if request.method == "GET":
-            response = JsonResponse({"message": id})
-            response.status_code = 404
-            return response
-        else:
-            product = None
-
+def api_show_product(request, id):
     if request.method == "GET":
+        products = Product.objects.get(id=id)
         return JsonResponse(
-            product,
+            {"products": products},
             encoder=ProductEncoder,
-            safe=False,
         )
     elif request.method == "PUT":
+        content = json.loads(request.body)
         try:
-            content = json.loads(request.body)
-        except json.JSONDecodeError:
-            response = JsonResponse({"message": "Bad JSON"})
-            response.status_code = 400
-            return response
-
-        if product is not None:
-            product.objects.filter(id=id).update(**content)
-            product = Product.objects.get(id=id)
-            status = 200
-            response_content = product
-        else:
-            status, response_content, product = create_product(request.body)
-        if product:
-            product.save()
-            send_product_data(product)
-        response = JsonResponse(
-            response_content,
+            if "category" in content:
+                category = Category.objects.get(id=content["category"])
+                content["category"] = category
+        except Category.DoesNotExist:
+               return JsonResponse(
+                {"message": "Invalid Category"},
+                status=400,
+            )
+        product = Product.objects.filter(id=id).update(**content)
+        product = Product.objects.get(id=id)
+        return JsonResponse(
+            product,
             encoder=ProductEncoder,
             safe=False,
         )
-        response.status_code = status
-        return response
     else:
-        product = Product.objects.filter(id=id).delete()
-        send_product_data(product)
-        response = HttpResponse()
-        response.status_code = 204
-        return response
+       count, _ = Product.objects.filter(id=id).delete()
+       return JsonResponse({"deleted": count > 0})
 
 @require_http_methods(["GET", "POST"])
 def api_list_categories(request):
@@ -169,3 +94,24 @@ def api_list_categories(request):
             encoder=CategoryEncoder,
             safe=False,
         )
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_show_category(request, id):
+    if request.method == "GET":
+        categories = Category.objects.get(id=id)
+        return JsonResponse(
+            {"categories": categories},
+            encoder=CategoryEncoder,
+        )
+    elif request.method == "PUT":
+        content = json.loads(request.body)
+        category = Category.objects.filter(id=id).update(**content)
+        category = Category.objects.get(id=id)
+        return JsonResponse(
+            category,
+            encoder=CategoryEncoder,
+            safe=False,
+        )
+    else:
+       count, _ = Category.objects.filter(id=id).delete()
+       return JsonResponse({"deleted": count > 0})
