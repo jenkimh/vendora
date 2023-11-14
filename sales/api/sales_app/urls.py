22714e4b453e059e09db1ad9e54af8ec4708a05d
Jeny <jeny.kim@yahoo.com>
2023-11-14 12:31:12 -0500

    sales urls

--- sales/api/sales_app/urls.py
+++ sales/api/sales_app/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 
-from sales_app.views import
+from sales_app.views import api_list_sales, api_show_sales
+
+urlpatterns = [
+    path("orders/", api_list_sales, name="api_list_sales"),
+    path(
+        "orders/<int:id>/",
+        api_show_sales,
+        name="api_show_sales"
+    )
+]
